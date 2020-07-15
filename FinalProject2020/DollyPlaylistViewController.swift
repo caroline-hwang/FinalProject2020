e365a0341072bbb5bd0b2969ee50a846692ae9e5
Caroline Hwnag <pandabear.penguin.h@gmail.com>
2020-07-15 11:12:31 -0400

    trying

---
+++ FinalProject2020/DollyPlaylistViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DollyPlaylistViewController.swift
+//  FinalProject2020
+//
+//  Created by Caroline Hwang on 7/15/20.
+//  Copyright © 2020 Caroline Hwang. All rights reserved.
+//
+
+import UIKit
+
+class DollyPlaylistViewController: UIViewController {
+
+    
+    @IBOutlet weak var dollyLink: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateTextView()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func updateTextView(){
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FinalProject2020/NSAttributedStringDolly.swift
@@ -0,0 +1,13 @@
+//
+//  NSAttributedStringDolly.swift
+//  FinalProject2020
+//
+//  Created by Caroline Hwang on 7/15/20.
+//  Copyright © 2020 Caroline Hwang. All rights reserved.
+//
+
+import Foundation
+
+extension NSAttributedString {
+    
+}
